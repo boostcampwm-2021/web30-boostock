66e2560052cec176e12bd510760725272cdd66ea
권구상 <52883945+rnjsrntkd95@users.noreply.github.com>
2021-11-02 16:58:25 +0900

    feat:종목 상세 정보 프레임 구현 (#35)
    종목 상세 정보 페이지의 프레임을 구성

--- front/src/pages/trade/Trade.tsx
+++ front/src/pages/trade/Trade.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Trade.scss';
 
 // interface Props {}
 
 const Trade = () => {
-	return <div />;
+	return (
+		<>
+			<header className="trade-header">header</header>
+			<main className="trade-container">
+				<aside className="aside-bar">aside</aside>
+				<section className="trade-body">
+					<section className="trade-info">info</section>
+					<section className="trade-chart">chart</section>
+					<section className="trade-status">
+						<section className="trade-order">order</section>
+						<section className="trade-bid-ask">bid-ask</section>
+					</section>
+					<section className="trade-conclusion">conclusion</section>
+				</section>
+			</main>
+		</>
+	);
 };
 
 export default Trade;
