b85c73705f2d226a457335d6923b40c1c664e995
SYN0P <20095859+SYN0P@users.noreply.github.com>
2021-11-02 18:05:30 +0900

    feat: 주문 관련 모델, 컨트롤러, 서비스 추가

---
+++ back/src/services/OrderService.ts
@@ -0,0 +1,85 @@
+import { getCustomRepository } from 'typeorm';
+import Order, { OrderStatus } from '@models/Order';
+import OrderRepository from '@repositories/OrderRepository';
+
+export default class OrderService {
+	static instance: OrderService | null = null;
+
+	protected orderRepository: OrderRepository | null =
+		getCustomRepository(OrderRepository);
+
+	constructor() {
+		if (OrderService.instance) return OrderService.instance;
+		OrderService.instance = this;
+	}
+
+	/* TODO: SEARCH METHOD
+	public async search(data: {
+		userId?: number;
+		stockId?: number;
+		type?: number;
+		amount?: number;
+		price?: number;
+		offset?: number;
+		limit?: number;
+	}): Promise<Order[]> {
+		if (!this.orderRepository) return [];
+		console.log(data);
+		await this.orderRepository.readOrdersByFilter(
+			{
+				user_id: data.userId,
+				stock_id: data.stockId,
+				type: data.type,
+				amount: data.amount,
+				price: data.price,
+			},
+			{
+				offset: data.offset,
+				limit: data.limit,
+			},
+		);
+		return [];
+	}
+    */
+
+	public async order(data: {
+		userId: number;
+		stockId: number;
+		type: number;
+		amount: number;
+		price: number;
+	}): Promise<void> {
+		if (!this.orderRepository) return;
+		const orderEntity = new Order();
+		orderEntity.user_id = data.userId;
+		orderEntity.stock_id = data.stockId;
+		orderEntity.type = data.type;
+		orderEntity.amount = data.amount;
+		orderEntity.price = data.price;
+		orderEntity.created_at = new Date();
+		orderEntity.status = OrderStatus.PENDING;
+		await this.orderRepository.createOrder(orderEntity);
+	}
+
+	public async cancel(orderId: number): Promise<void> {
+		if (!this.orderRepository) return;
+		await this.orderRepository.deleteOrder(orderId);
+	}
+
+	public async modify(
+		orderId: number,
+		data: { amount?: number; price?: number },
+	): Promise<void> {
+		if (!this.orderRepository) return;
+		if (data.amount && data.price)
+			await this.orderRepository.updateOrder(orderId, data);
+		else if (data.amount)
+			await this.orderRepository.updateOrder(orderId, {
+				amount: data.amount,
+			});
+		else if (data.price)
+			await this.orderRepository.updateOrder(orderId, {
+				price: data.price,
+			});
+	}
+}
