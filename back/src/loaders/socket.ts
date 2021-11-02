2e64107d9acda45a2c63d7c6a4e6d3d15af79812
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2021-11-02 18:36:27 +0900

    feat:웹소켓 커넥션 구현
    클라이언트가 서버의 소켓에 연결 시 일정 시간마다 data를 전송하도록 합니다
    종목 정보의 인터페이스를 구성하여 데이터 형식을 임시로 정의합니다

--- back/src/loaders/socket.ts
+++ back/src/loaders/socket.ts
@@ -2,6 +2,10 @@ import wsModule from 'ws';
 import express from 'express';
 import Logger from './logger';
 
+import { IStockInformation } from '../interfaces/stockInformation';
+
+const translateSocketData = (data) => JSON.stringify(data);
+
 export default (app: express.Application): void => {
 	const HTTPServer = app.listen(3333, () => {
 		Logger.info('✌️ Socket loaded at port:3333');
@@ -9,5 +13,13 @@ export default (app: express.Application): void => {
 
 	const webSocketServer = new wsModule.Server({ server: HTTPServer });
 
-	webSocketServer.on('connection', (ws, req) => {});
+	webSocketServer.on('connection', (ws, req) => {
+		setInterval(() => {
+			const data: IStockInformation = {
+				name: 'Boostock',
+				current: 3000,
+			};
+			ws.send(translateSocketData(data));
+		}, 1000);
+	});
 };
