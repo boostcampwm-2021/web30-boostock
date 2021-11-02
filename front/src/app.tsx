507b54a1224d1291d64fec45a1dee8d43c76aa3a
nawhes <nawhes@naver.com>
2021-11-02 17:07:18 +0900

    style: 테스트를 위해 구성한 내용 제거

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -2,15 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import './app.scss';
-import style from './app.module.scss';
 
 const App = () => {
-	return (
-		<h1 className={style.test}>
-			<div> TTT </div>
-			Hello World!
-		</h1>
-	);
+	return <h1>Hello World!</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
