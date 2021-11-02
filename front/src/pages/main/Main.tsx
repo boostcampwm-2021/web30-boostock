301402ae95f84e8e517feaea055169e91f5b6941
SYN0P <20095859+SYN0P@users.noreply.github.com>
2021-11-02 12:18:49 +0900

    feat:페이지 템플릿 추가

---
+++ front/src/pages/main/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// interface Props {}
+
+const Main = () => {
+	return <div />;
+};
+
+export default Main;
---
+++ front/src/pages/signUp/SignUp.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// interface Props {}
+
+const SignUp = () => {
+	return <div />;
+};
+
+export default SignUp;
