bf9912173a532f3c69af836a38347fc5cfbee92a
SYN0P <20095859+SYN0P@users.noreply.github.com>
2021-11-03 18:58:57 +0900

    refactor: 주문 상세 페이지에 구현된 주문 정보 추가

--- front/src/pages/trade/Trade.tsx
+++ front/src/pages/trade/Trade.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import StockInfo, { Info } from './stockInfo/StockInfo';
 import SideBar from './sideBar/SideBar';
 import BidAsk from './bidAsk/BidAsk';
 import './Trade.scss';
 
 // interface Props {}
+
+const info: Info = {
+	name: '호눅스 코인',
+	price: 1234567,
+	percent: 1234,
+	high: 1234,
+	low: 1234,
+	amount: 1234,
+	volume: 1234567,
+};
 
 const Trade = () => {
 	return (
@@ -13,7 +24,9 @@ const Trade = () => {
 					<SideBar />
 				</aside>
 				<section className="trade-body">
-					<section className="trade-info">info</section>
+					<section className="trade-info">
+						<StockInfo info={info} />
+					</section>
 					<section className="trade-chart">chart</section>
 					<section className="trade-status">
 						<section className="trade-order">order</section>
