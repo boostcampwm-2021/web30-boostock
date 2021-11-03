cc7be6dc3fe933ef29f28dcd18017259685c9200
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2021-11-03 16:16:00 +0900

    refactor:레이아웃 안의 스타일 일치화, 소켓 로더 코드 위치 변경
    종목 상세정보 페이지 내의 컴포넌트들의 위치와 그림자를 수정합니다
    소켓 로더의 위치를 express 로더에서 index 로더로 변경합니다

--- back/src/loaders/index.ts
+++ back/src/loaders/index.ts
@@ -1,12 +1,13 @@
 import expressLoader from './express';
 import typeormLoader from './typeorm';
+import webSocketLoader from './socket';
+
 import Logger from './logger';
 
-export default async ({ expressApp, http }) => {
+export default async ({ expressApp, http }): Promise<void> => {
 	await expressLoader({ app: expressApp });
 	Logger.info('✌️ Express loaded');
 	await typeormLoader();
 	Logger.info('✌️ Typeorm loaded');
-
-	// ... more loaders can be here
+	webSocketLoader(expressApp);
 };
