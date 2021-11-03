17d469b9cfde742d6655ae2682066c6605a1d402
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2021-11-03 17:49:35 +0900

    feat:클라이언트가 소켓에 접속 시 특정 종목 정보를 전달
    클라이언트가 종목 상세 페이지에 접근 시 렌더링할 종목을 서버에게 전달합니다
    서버는 소켓에 새로운 클라이언트가 연결되면 응답할 종목을 Map으로 저장합니다

--- back/src/loaders/socket.ts
+++ back/src/loaders/socket.ts
@@ -3,8 +3,20 @@ import express from 'express';
 import Logger from './logger';
 
 import { IStockInformation } from '../interfaces/stockInformation';
+import { ISocketRequest } from '../interfaces/socketRequest';
 
-const translateSocketData = (data) => JSON.stringify(data);
+const socketClientMap = new Map();
+const stockInformation: Map<string, IStockInformation> = new Map([
+	[
+		'Boostock',
+		{
+			name: 'Boostock',
+			current: 3000,
+		},
+	],
+]);
+const translateRequestFormat = (data) => JSON.parse(data);
+const translateResponseFormat = (data) => JSON.stringify(data);
 
 export default (app: express.Application): void => {
 	const HTTPServer = app.listen(3333, () => {
@@ -12,17 +24,27 @@ export default (app: express.Application): void => {
 	});
 
 	const webSocketServer = new wsModule.Server({ server: HTTPServer });
-	const broadcast = (message: string) => {
-		webSocketServer.clients.forEach((client) => {
-			client.send(message);
+	const broadcast = (clients) => {
+		clients.forEach((stock, client) => {
+			const response = stockInformation.get(stock);
+			client.send(translateResponseFormat(response));
 		});
 	};
-
 	setInterval(() => {
-		const message: IStockInformation = {
-			name: 'Boostock',
-			current: 3000,
-		};
-		broadcast(translateSocketData(message));
+		broadcast(socketClientMap);
 	}, 1000);
+
+	webSocketServer.on('connection', (ws, req) => {
+		const clientIP =
+			req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+		ws.on('message', (message: string) => {
+			const requestData: ISocketRequest = translateRequestFormat(
+				message,
+			) || { type: 'error' };
+			if (requestData.type === 'visited') {
+				socketClientMap.set(ws, requestData.stock);
+			}
+		});
+	});
 };
