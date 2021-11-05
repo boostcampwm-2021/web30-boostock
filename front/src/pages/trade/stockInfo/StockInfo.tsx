1739c5d4357a8bc1e8c0117697f5229d11dc4461
jaehyeon48 <imjaehyeon48@gmail.com>
2021-11-05 22:43:38 +0900

    refactor: stockInfo 컴포넌트 UI 변경

--- front/src/pages/trade/stockInfo/StockInfo.tsx
+++ front/src/pages/trade/stockInfo/StockInfo.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { AiFillCaretUp, AiFillCaretDown } from 'react-icons/ai';
 import { IStockListItem } from '@recoil/stockList/index';
+import formatNumber from '@src/common/utils/formatNumber';
 
 import './StockInfo.scss';
 
 interface Props {
 	info: IStockListItem;
+}
+
+function priceColorClass(percent: number): string {
+	if (percent < 0) return 'price-minus';
+	if (percent > 0) return 'price-plus';
+	return 'price-neutral';
+}
+
+function caretIcon(percent: number) {
+	if (percent < 0) return <AiFillCaretDown />;
+	if (percent > 0) return <AiFillCaretUp />;
+	return '';
+}
+
+function formatTradingData(data: number, postfix: string) {
+	const ONE_THOUSAND = 1_000;
+	const ONE_MILLION = 1_000_000;
+	const ONE_BILLION = 1_000_000_000;
+
+	if (data < ONE_MILLION) return `${formatNumber(data)}${postfix}`;
+	if (data < ONE_BILLION)
+		return `${formatNumber(Math.round(data / ONE_THOUSAND))}천${postfix}`;
+	return `${formatNumber(Math.round(data / ONE_MILLION))}백만${postfix}`;
 }
 
 const StockInfo = (props: Props) => {
@@ -21,49 +46,41 @@ const StockInfo = (props: Props) => {
 
 	const percent =
 		((currentPrice - previousClosingPrice) / previousClosingPrice) * 100;
-	let status = '';
-	if (percent === 0) status = '';
-	else if (percent > 0) status = 'up';
-	else if (percent < 0) status = 'down';
 
 	return (
 		<div className="stock-info">
-			<div className="stock-info__top ">
-				<p className="stock-info__top-name">{name}</p>
-				<div className={`stock-info__top-price ${status}`}>
-					<p className="stock-info__top-price-value">
-						{currentPrice.toLocaleString()}
-					</p>
-					<p className="stock-info__top-price-percent">
-						{percent > 0 ? '+' : ''}
-						{percent.toFixed(1)}%
-					</p>
+			<div className="stock-info__left">
+				<header className="stock-name">{name}</header>
+				<div
+					className={`current-price-info ${priceColorClass(percent)}`}
+				>
+					<div className="current-price ">
+						₩{formatNumber(currentPrice)}
+					</div>
+					<div className="price-percent">
+						{caretIcon(percent)}
+						{percent.toFixed(2)}%
+					</div>
 				</div>
 			</div>
-			<div className="stock-info__bottom">
-				<div className="stock-info__bottom-data stock-info__bottom-data--up">
-					<p className="stock-info__bottom-data-key">고가</p>
-					<p className="stock-info__bottom-data-value">
-						{highPrice.toLocaleString()}
-					</p>
+			<div className="stock-info__right">
+				<div className="extra-info highest-price">
+					<span className="extra-info-data">
+						{formatNumber(lowPrice)}원 ~ {formatNumber(highPrice)}원
+					</span>
+					<span className="extra-info-text">일일 가격 범위</span>
 				</div>
-				<div className="stock-info__bottom-data  stock-info__bottom-data--down">
-					<p className="stock-info__bottom-data-key">저가</p>
-					<p className="stock-info__bottom-data-value">
-						{lowPrice.toLocaleString()}
-					</p>
+				<div className="extra-info trading-volume">
+					<span className="extra-info-data">
+						{formatTradingData(tradingVolume, '주')}
+					</span>
+					<span className="extra-info-text">거래량</span>
 				</div>
-				<div className="stock-info__bottom-data">
-					<p className="stock-info__bottom-data-key">거래량</p>
-					<p className="stock-info__bottom-data-value">
-						{tradingVolume.toLocaleString()} 주
-					</p>
-				</div>
-				<div className="stock-info__bottom-data">
-					<p className="stock-info__bottom-data-key">거래대금</p>
-					<p className="stock-info__bottom-data-value">
-						{tradingAmount.toLocaleString()} 원
-					</p>
+				<div className="extra-info trading-amount">
+					<span className="extra-info-data">
+						{formatTradingData(tradingAmount, '원')}
+					</span>
+					<span className="extra-info-text">거래대금</span>
 				</div>
 			</div>
 		</div>
