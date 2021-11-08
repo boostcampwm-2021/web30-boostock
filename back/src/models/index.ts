712dfd91209576f0e1899e7f7fed36675a6ae0e0
SYN0P <20095859+SYN0P@users.noreply.github.com>
2021-11-09 03:34:01 +0900

    chore: 인덱싱 추가

--- back/src/models/index.ts
+++ back/src/models/index.ts
@@ -1,3 +1,14 @@
-export * as User from './User';
-export * as Stock from './Stock';
-export * as Order from './Order';
+export * from './User';
+export { default as User } from './User';
+
+export * from './UserFavorite';
+export { default as UserFavorite } from './UserFavorite';
+
+export * from './UserStock';
+export { default as UserStock } from './UserStock';
+
+export * from './Stock';
+export { default as Stock } from './Stock';
+
+export * from './Order';
+export { default as Order } from './Order';
--- back/src/repositories/index.ts
+++ back/src/repositories/index.ts
@@ -1,1 +1,14 @@
-export * as UserRepository from './UserRepository';
+export * from './UserRepository';
+export { default as UserRepository } from './UserRepository';
+
+export * from './UserFavoriteRepository';
+export { default as UserFavoriteRepository } from './UserFavoriteRepository';
+
+export * from './UserStockRepository';
+export { default as UserStockRepository } from './UserStockRepository';
+
+export * from './StockRepository';
+export { default as StockRepository } from './StockRepository';
+
+export * from './OrderRepository';
+export { default as OrderRepository } from './OrderRepository';
