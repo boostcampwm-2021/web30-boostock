9b85a185f8afbcd33fb3b13a4105e5f7cfe35dc2
SYN0P <20095859+SYN0P@users.noreply.github.com>
2021-11-09 03:34:01 +0900

    chore: 인덱싱 추가

---
+++ back/src/services/index.ts
@@ -0,0 +1,14 @@
+export * from './UserService';
+export { default as UserService } from './UserService';
+
+export * from './UserFavoriteService';
+export { default as UserFavoriteService } from './UserFavoriteService';
+
+export * from './UserStockService';
+export { default as UserStockService } from './UserStockService';
+
+export * from './StockService';
+export { default as StockService } from './StockService';
+
+export * from './OrderService';
+export { default as OrderService } from './OrderService';
