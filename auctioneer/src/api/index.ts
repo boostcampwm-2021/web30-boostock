444fa4227d33b19cba91a2bcc0ca1983c487fcac
SYN0P <20095859+SYN0P@users.noreply.github.com>
2021-11-10 00:05:16 +0900

    fix: 삭제된 라우터 인덱싱 제거

--- auctioneer/src/api/index.ts
+++ auctioneer/src/api/index.ts
@@ -1,11 +1,6 @@
 import express from 'express';
 
-import user from './user/index';
-import order from './order/index';
-
 export default (): express.Router => {
 	const router = express.Router();
-	router.use('/user', user());
-	router.use('/order', order());
 	return router;
 };
