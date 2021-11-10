85342ff9858df7b1807d4cfa04152a869ccc3958
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2021-11-10 15:12:12 +0900

    feat:Event Emitter 생성, broadcast 이벤트 추가
    api에서 broadcast 이벤트를 호출하여 클라이언트들에게 데이터를 전송할 수 있도록 합니다.

--- back/src/api/stock/stock.ts
+++ back/src/api/stock/stock.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from 'express';
+import Emitter from '../../helper/eventEmitter';
 // import User from '@models/User';
-import StockService from '@services/StockService';
+// import StockService from '@services/StockService';
 
 export default (): express.Router => {
 	const router = express.Router();
+	router.get('/', (req: Request, res: Response) => {
+		console.log('라우터 진입');
+		Emitter.emit('broadcast');
+	});
+
 	return router;
 };
--- back/src/loaders/socket.ts
+++ back/src/loaders/socket.ts
@@ -2,18 +2,18 @@ import wsModule from 'ws';
 import express from 'express';
 import Logger from './logger';
 import moment from '../helper/moment';
+
 import { IStockInformation } from '../interfaces/stockInformation';
 import { ISocketRequest } from '../interfaces/socketRequest';
 
 import StockService from '../services/StockService';
+import Emitter from '../helper/eventEmitter';
 
 const socketClientMap = new Map();
 const translateRequestFormat = (data) => JSON.parse(data);
 const translateResponseFormat = (type, data) => JSON.stringify({ type, data });
 
-export default (
-	app: express.Application,
-): { broadcast: (stock: string, msg) => void } => {
+export default (app: express.Application) => {
 	const HTTPServer = app.listen(process.env.SOCKET_PORT || 3333, () => {
 		Logger.info(
 			`✌️ Socket loaded at port:${process.env.SOCKET_PORT || 3333}`,
@@ -23,21 +23,22 @@ export default (
 	const broadcast = (stockCode, msg) => {
 		webSocketServer.clients.forEach((client) => {
 			const targetStockCode = socketClientMap.get(client);
-			if (targetStockCode === stockCode) {
-				// 모든 데이터 전송, 현재가, 호가, 차트 등...
-				client.send(msg);
-			} else {
-				// msg 오브젝트의 데이터에서 aside 바에 필요한 데이터만 골라서 전송
-				client.send(msg.data);
-			}
+			client.send(translateResponseFormat('test', 'testdata'));
+			// if (targetStockCode === stockCode) {
+			// 	// 모든 데이터 전송, 현재가, 호가, 차트 등...
+			// 	client.send(msg);
+			// } else {
+			// 	// msg 오브젝트의 데이터에서 aside 바에 필요한 데이터만 골라서 전송
+			// 	client.send(msg.data);
+			// }
 		});
 	};
+	Emitter.on('broadcast', broadcast);
 
 	webSocketServer.on('connection', (ws, req) => {
 		socketClientMap.set(ws, '');
 		const stockService = new StockService();
 		const stockList = stockService.getStocksCurrent();
-		console.log('유저 접속');
 		ws.send(translateResponseFormat('stocks_info', stockList));
 
 		ws.on('message', (message: string) => {
@@ -61,7 +62,4 @@ export default (
 
 		ws.on('close', () => {});
 	});
-	return {
-		broadcast,
-	};
 };
