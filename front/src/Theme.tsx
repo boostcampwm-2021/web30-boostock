5048302d3ddd1c798c407d2e3da89b7d8560abab
jaehyeon48 <imjaehyeon48@gmail.com>
2021-11-11 23:13:15 +0900

    fix: 다크 모드로 변경할 때 소켓 재연결되는 현상 수정

---
+++ front/src/Theme.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import userAtom from '@recoil/user/index';
+
+interface IProps {
+	children: React.ReactNode;
+}
+
+const Theme = ({ children }: IProps) => {
+	const { theme } = useRecoilValue(userAtom);
+
+	return <main className={theme === 'light' ? 'theme-container' : 'theme-container dark-theme'}>{children}</main>;
+};
+
+export default Theme;
