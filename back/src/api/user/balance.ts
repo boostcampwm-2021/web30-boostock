fb81af437eeeaa54ff7cc8d7d53ccc318ae70cdd
nawhes <nawhes@naver.com>
2021-11-17 18:16:14 +0900

    feat: API 유저 잔액 조회 등 기타 기능

---
+++ back/src/api/user/balance.ts
@@ -0,0 +1,114 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { AuthError, AuthErrorMessage, ParamError, ParamErrorMessage } from '@services/errors/index';
+import { UserService } from '@services/index';
+import UserBalance, { IBalanceHistory } from '@models/UserBalance';
+
+const COUNT_PER_PAGE = 30;
+
+enum BalanceTransperType {
+	WITHDRAW = 0,
+	DEPOSIT = 1,
+}
+
+enum BalanceTransperStatus {
+	PENDING = 0,
+	FINISHED = 1,
+}
+
+export default (): express.Router => {
+	const router = express.Router();
+	router.get('/balance', async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const userId = req.session.data?.userId;
+			if (userId === undefined) throw new AuthError(AuthErrorMessage.INVALID_SESSION);
+			const page = req.body.page ? req.body.page * COUNT_PER_PAGE * -1 : 0;
+			const result = await UserService.getUserById(userId);
+			const { balance } = result;
+			const document = await UserBalance.findOne({ userId }, { balanceHistory: { $slice: [page, COUNT_PER_PAGE] } });
+			const history = document?.balanceHistory;
+			res.status(200).json({ balance, history });
+		} catch (error) {
+			next(error);
+		}
+	});
+
+	router.post('/balance/deposit', async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const userId = req.session.data?.userId;
+			if (userId === undefined) throw new AuthError(AuthErrorMessage.INVALID_SESSION);
+			const { bank, bankAccount } = req.body;
+			let { changeValue } = req.body;
+			changeValue = Number(changeValue);
+			if (changeValue === undefined || Number.isNaN(changeValue) || bank === undefined || bankAccount === undefined)
+				throw new ParamError(ParamErrorMessage.INVALID_PARAM);
+			const result = await UserService.updateBalance(userId, changeValue);
+			const { balance } = result;
+			res.status(200).json({ balance });
+
+			const newBalanceHistory: IBalanceHistory = {
+				type: BalanceTransperType.DEPOSIT,
+				volume: changeValue < 0 ? changeValue * -1 : changeValue,
+				status: BalanceTransperStatus.FINISHED,
+				bank,
+				bankAccount,
+				createdAt: new Date().getTime(),
+			};
+
+			const document = await UserBalance.findOne({ userId });
+			if (document) {
+				document.balanceHistory.push(newBalanceHistory);
+				document.save((err) => {
+					if (err) throw err;
+				});
+			} else {
+				const newDocument = new UserBalance({
+					userId,
+				});
+				newDocument.balanceHistory.push(newBalanceHistory);
+				newDocument.save();
+			}
+		} catch (error) {
+			next(error);
+		}
+	});
+
+	router.post('/balance/withdraw', async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const userId = req.session.data?.userId;
+			if (userId === undefined) throw new AuthError(AuthErrorMessage.INVALID_SESSION);
+			const { changeValue, bank, bankAccount } = req.body;
+			if (changeValue === undefined || Number.isNaN(Number(changeValue)) || bank === undefined || bankAccount === undefined)
+				throw new ParamError(ParamErrorMessage.INVALID_PARAM);
+			const result = await UserService.updateBalance(userId, changeValue);
+			const { balance } = result;
+			res.status(200).json({ balance });
+
+			const newBalanceHistory: IBalanceHistory = {
+				type: BalanceTransperType.WITHDRAW,
+				volume: changeValue < 0 ? changeValue * -1 : changeValue,
+				status: BalanceTransperStatus.FINISHED,
+				bank,
+				bankAccount,
+				createdAt: new Date().getTime(),
+			};
+
+			const document = await UserBalance.findOne({ userId });
+			if (document) {
+				document.balanceHistory.push(newBalanceHistory);
+				document.save((err) => {
+					if (err) throw err;
+				});
+			} else {
+				const newDocument = new UserBalance({
+					userId,
+				});
+				newDocument.balanceHistory.push(newBalanceHistory);
+				newDocument.save();
+			}
+		} catch (error) {
+			next(error);
+		}
+	});
+
+	return router;
+};
---
+++ back/src/models/UserBalance.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+export interface IBalanceHistory {
+	type: number;
+	volume: number;
+	status: number;
+	bank: string;
+	bankAccount: string;
+	createdAt: number;
+}
+
+export interface IUserBalance {
+	userId: number;
+	balanceHistory: IBalanceHistory[];
+}
+
+export const BalanceHistorySchema = new Schema<IBalanceHistory>({
+	type: { type: Number, required: true },
+	volume: { type: Number, required: true },
+	status: { type: Number, required: true },
+	bank: { type: String, required: true },
+	bankAccount: { type: String, required: true },
+	createdAt: { type: Number, required: true },
+});
+
+export const UserBalanceSchema = new Schema<IUserBalance>(
+	{
+		userId: { type: Number, required: true },
+		balanceHistory: [BalanceHistorySchema],
+	},
+	{ collection: 'UserBalance' },
+);
+
+export default mongoose.model('UserBalance', UserBalanceSchema);
