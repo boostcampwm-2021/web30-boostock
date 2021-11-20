6926345e7d20a2beb5db9242a10bc6d211530db5
jaehyeon48 <imjaehyeon48@gmail.com>
2021-11-20 14:22:10 +0900

    style: 로그인 됐을 때의 navbar 스타일 수정
    유저 아이콘 추가, 아이콘 클릭 시 드롭다운 메뉴가 보이도록 합니다

--- front/src/common/topbar/menu/Menu.tsx
+++ front/src/common/topbar/menu/Menu.tsx
@@ -1,39 +1,76 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import { Ipage } from '@src/app';
 import User from '@recoil/user/index';
+import UserIcon from './UserIcon';
 
-import style from './menu.module.scss';
+import './menu.scss';
 
 interface Props {
 	pages: Ipage[];
 }
 
+function userIconClass(isDropdownOpen: boolean): string {
+	if (isDropdownOpen) return 'navbar__user open';
+	return 'navbar__user';
+}
+
 const Menu = ({ pages }: Props) => {
 	const userState = useRecoilValue(User);
+	const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+	const handleToggleDropdownOpen = () => {
+		setIsDropdownOpen(!isDropdownOpen);
+	};
+
+	useEffect(() => {
+		const handleCloseDropdown = (e: Event) => {
+			const target = e.target as HTMLElement;
+			if (target.closest('.navbar__user-icon')) return;
+			setIsDropdownOpen(false);
+		};
+
+		document.addEventListener('click', handleCloseDropdown);
+
+		return () => {
+			document.removeEventListener('click', handleCloseDropdown);
+		};
+	}, []);
 
 	const getMenu = () => {
 		if (userState.isLoggedIn === true) {
 			return (
-				<div style={{ position: 'relative' }}>
-					<NavLink to="/my" className={(isActive) => (isActive ? `${style.active}` : '')}>
-						마이페이지
-					</NavLink>
-					<NavLink to="/balance" className={(isActive) => (isActive ? `${style.active}` : '')}>
-						입출금
-					</NavLink>
+				<div className={userIconClass(isDropdownOpen)}>
+					<button type="button" className="navbar__user-icon" onClick={handleToggleDropdownOpen}>
+						<UserIcon />
+					</button>
+					{isDropdownOpen && (
+						<ul className="navbar__user-dropdown-list">
+							<li className="navbar__user-dropdown-item">
+								<Link to="/my">마이페이지</Link>
+							</li>
+							<li className="navbar__user-dropdown-item">
+								<Link to="/balance">입출금</Link>
+							</li>
+							<li className="navbar__user-dropdown-item">
+								<button type="button" className="logout-btn">
+									로그아웃
+								</button>
+							</li>
+						</ul>
+					)}
 				</div>
 			);
 		}
 
 		return (
-			<div style={{ position: 'relative' }}>
-				<NavLink to="/auth/signin" className={(isActive) => (isActive ? `${style.active}` : '')}>
+			<div className="auth-actions">
+				<NavLink to="/auth/signin" className="navbar__signin">
 					로그인
 				</NavLink>
-				<NavLink to="/auth/signup" className={(isActive) => (isActive ? `${style.active}` : '')}>
+				<NavLink to="/auth/signup" className="navbar__signup">
 					회원가입
 				</NavLink>
 			</div>
@@ -41,10 +78,10 @@ const Menu = ({ pages }: Props) => {
 	};
 
 	return (
-		<nav className={style.container}>
+		<nav className="container">
 			<div>
 				{pages.map((page) => (
-					<NavLink to={page.url} key={page.id} className={(isActive) => (isActive ? `${style.active}` : '')}>
+					<NavLink to={page.url} key={page.id} className={(isActive) => (isActive ? 'active' : '')}>
 						{page.title}
 					</NavLink>
 				))}
