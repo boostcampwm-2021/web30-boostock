bc1ef5554a8c53cb77c8dc91c9efb81fc192af41
jaehyeon48 <imjaehyeon48@gmail.com>
2021-11-24 16:24:20 +0900

    feat: 차트 왼쪽/오른쪽 슬라이드 기능 구현

--- front/src/pages/trade/chart/Chart.tsx
+++ front/src/pages/trade/chart/Chart.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState, useEffect, useRef } from 'react';
 import { useRecoilState } from 'recoil';
-import ChartAtom from '@src/recoil/chart/atom';
+import ChartAtom, { IChartItem } from '@src/recoil/chart/atom';
 import { ICrossLine } from './common';
 
 import PeriodBackground from './PeriodBackground';
@@ -12,6 +12,9 @@ import PeriodLegend from './PeriodLegend';
 import './Chart.scss';
 
 const NUM_OF_CANDLES = 60;
+const DEFAULT_START_INDEX = 0;
+const DEFAULT_END_INDEX = 60;
+const MOVE_INDEX_SLOW_WEIGHT = 4;
 
 const moveCrossLine = (set: React.Dispatch<React.SetStateAction<ICrossLine>>, event: MouseEvent) => {
 	set(() => ({
@@ -33,9 +36,9 @@ const chartContainerClass = (isUserGrabbing: boolean) => {
 const Chart = () => {
 	const chartRef = useRef<HTMLDivElement>(null);
 	const [isUserGrabbing, setIsUserGrabbing] = useState<boolean>(false);
-	const [chart, setChart] = useRecoilState(ChartAtom);
-	const [start, setStart] = useState<number>(0); // 맨 오른쪽 캔들의 인덱스
-	const [end, setEnd] = useState<number>(60); // 맨 왼쪽 캔들의 인덱스
+	const [chart, setChart] = useRecoilState<IChartItem[]>(ChartAtom);
+	const [start, setStart] = useState<number>(DEFAULT_START_INDEX); // 맨 오른쪽 캔들의 인덱스
+	const [end, setEnd] = useState<number>(DEFAULT_END_INDEX); // 맨 왼쪽 캔들의 인덱스
 
 	const [crossLine, setCrossLine] = useState<ICrossLine>({ event: null, posX: 0, posY: 0 });
 
@@ -47,6 +50,10 @@ const Chart = () => {
 			chartRef.current?.removeEventListener('mousemove', bindedMoveCrossLine);
 		};
 	}, []);
+
+	if (chart.length === 0) {
+		return <p>차트 데이터가 없습니다.</p>;
+	}
 
 	return (
 		<div
@@ -65,12 +72,28 @@ const Chart = () => {
 			onMouseMove={(e) => {
 				if (!isTarget(e.target as HTMLDivElement)) return;
 				if (!isUserGrabbing) return;
+
+				const moveIndex = Math.floor(e.movementX / MOVE_INDEX_SLOW_WEIGHT);
+				setStart((prev) => {
+					const newIndex = prev + moveIndex;
+					const numOfChartItems = chart.length;
+					if (numOfChartItems - 1 < newIndex) return numOfChartItems - 1;
+
+					return newIndex >= DEFAULT_START_INDEX ? newIndex : DEFAULT_START_INDEX;
+				});
+				setEnd((prev) => {
+					const newIndex = prev + moveIndex;
+					const numOfChartItems = chart.length;
+					if (DEFAULT_END_INDEX + numOfChartItems - 1 < newIndex) return DEFAULT_END_INDEX + numOfChartItems - 1;
+
+					return newIndex >= DEFAULT_END_INDEX ? newIndex : DEFAULT_END_INDEX;
+				});
 			}}
 		>
-			<PeriodBackground chartData={chart.slice(start, end + 1)} crossLine={crossLine} />
-			<CandleGraph chartData={chart.slice(start, end + 1)} numOfCandles={NUM_OF_CANDLES} crossLine={crossLine} />
-			<VolumeGraph chartData={chart.slice(start, end + 1)} crossLine={crossLine} />
-			<PeriodLegend chartData={chart.slice(start, end + 1)} crossLine={crossLine} />
+			<PeriodBackground chartData={chart.slice(start, end)} crossLine={crossLine} />
+			<CandleGraph chartData={chart.slice(start, end)} numOfCandles={NUM_OF_CANDLES} crossLine={crossLine} />
+			<VolumeGraph chartData={chart.slice(start, end)} crossLine={crossLine} />
+			<PeriodLegend chartData={chart.slice(start, end)} crossLine={crossLine} />
 			<div className="chart-menu" />
 		</div>
 	);
