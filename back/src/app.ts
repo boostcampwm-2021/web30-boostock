8230226a19475dbf9cf1c6c5af32e7231db530fe
SYN0P <20095859+SYN0P@users.noreply.github.com>
2021-11-26 16:10:41 +0900

    Revert "배포 환경에서 https 모듈 사용  및 SSL 적용 (#188)"
    This reverts commit 31a7ae84b82ae31cbb2f1936a48868768fc85824.

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,39 +1,11 @@
 import express from 'express';
-import fs from 'fs';
 import _http from 'http';
-import _https from 'https';
 
 import Logger from '@loaders/logger';
 import loaders from '@loaders/index';
 import config from '@config/index';
 
-async function startHttpsServer() {
-	const app = express();
-	const https = _https.createServer(
-		{
-			ca: fs.readFileSync('/etc/ssl/ca_bundle.crt'),
-			key: fs.readFileSync('/etc/ssl/private.key'),
-			cert: fs.readFileSync('/etc/ssl/certificate.crt'),
-		},
-		app,
-	);
-	await loaders({ expressApp: app });
-
-	https
-		.listen(config.port, () => {
-			Logger.info(`
-    	################################################
-    	🛡️  Server listening on port: ${config.port} 🛡️
-    	################################################
-      `);
-		})
-		.on('error', (err) => {
-			Logger.error(err);
-			process.exit(1);
-		});
-}
-
-async function startHttpServer() {
+async function startServer() {
 	const app = express();
 	const http = _http.createServer(app);
 	await loaders({ expressApp: app });
@@ -50,5 +22,4 @@ async function startHttpServer() {
 	});
 }
 
-if (process.env.NODE_ENV === 'production') startHttpsServer();
-else startHttpServer();
+startServer();
