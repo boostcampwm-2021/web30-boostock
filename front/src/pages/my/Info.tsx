3a0d7f853b93a5e5aad7a7a89a8adca307514e8e
jaehyeon48 <imjaehyeon48@gmail.com>
2021-12-02 13:07:04 +0900

    refactor: Info 컴포넌트 리팩토링
    1. useEffect 내에서 fetch를 사용하던 부분 수정
    2. 동적으로 css 클래스를 적용하는 로직 함수로 분리
    3. total 값을 계산하는 로직 함수로 분리

--- front/src/pages/my/Info.tsx
+++ front/src/pages/my/Info.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import formatInteger from '@src/common/utils/formatInteger';
 import { IUserHoldItem } from '@src/types';
 import { ONE_MONTH_IN_MILLISECONDS } from '@common/constants';
+import { getBalance } from '@lib/api';
 import './Info.scss';
 
 interface IInfo {
@@ -11,54 +12,51 @@ interface IInfo {
 	totalValuationProfit: number;
 	totalAssets: number;
 	totalRate: number;
-	status: string;
 }
 
-interface InfoProps {
+interface IProps {
 	holds: IUserHoldItem[];
 }
 
-const Info = (props: InfoProps) => {
-	const { holds } = props;
+const infoDataClass = (baseClass: string, value: number) => {
+	if (value > 0) baseClass += ' my-info__data--up';
+	if (value < 0) baseClass += ' my-info__data--down';
+	return baseClass;
+};
+
+const calculateTotalValueOf = (propToCalculate: keyof IUserHoldItem, holds: IUserHoldItem[]) => {
+	if (holds.length === 0) return 0;
+	if (typeof holds[0][propToCalculate] === 'string') return 0;
+
+	return holds.reduce((acc, current) => acc + (current[propToCalculate] as number), 0);
+};
+
+const Info = ({ holds }: IProps) => {
 	const [info, setInfo] = useState<IInfo | null>(null);
 
 	useEffect(() => {
 		const currentTime = Date.now();
 		const beforeTime = currentTime - ONE_MONTH_IN_MILLISECONDS;
-		fetch(`${process.env.SERVER_URL}/api/user/balance?start=${beforeTime}&end=${currentTime}`, {
-			method: 'GET',
-			credentials: 'include',
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8',
-			},
-		}).then((res: Response) => {
-			if (res.ok) {
-				res.json().then((data) => {
-					const { balance } = data;
-					const totalAskPrice = holds.reduce((prev, hold) => prev + hold.totalAskPrice, 0);
-					const totalValuationPrice = holds.reduce((prev, hold) => prev + hold.totalValuationPrice, 0);
-					const totalValuationProfit = totalValuationPrice - totalAskPrice;
+		(async () => {
+			const balanceData = await getBalance(beforeTime, currentTime);
+			if (!balanceData) return;
 
-					const totalAssets = balance + totalValuationPrice;
-					const totalRate = (totalValuationPrice / totalAskPrice) * 100 - 100 || 0;
+			const { balance } = balanceData;
+			const totalAskPrice = calculateTotalValueOf('totalAskPrice', holds);
+			const totalValuationPrice = calculateTotalValueOf('totalValuationPrice', holds);
+			const totalValuationProfit = totalValuationPrice - totalAskPrice;
+			const totalAssets = balance + totalValuationPrice;
+			const totalRate = (totalValuationPrice / totalAskPrice) * 100 - 100 || 0;
 
-					let status = '';
-					if (totalRate === 0) status = '';
-					else if (totalRate > 0) status = ' my-info__data--up';
-					else if (totalRate < 0) status = ' my-info__data--down';
-
-					setInfo({
-						balance,
-						totalAskPrice,
-						totalValuationPrice,
-						totalValuationProfit,
-						totalAssets,
-						totalRate,
-						status,
-					});
-				});
-			}
-		});
+			setInfo({
+				balance,
+				totalAskPrice,
+				totalValuationPrice,
+				totalValuationProfit,
+				totalAssets,
+				totalRate,
+			});
+		})();
 	}, [holds]);
 
 	return (
@@ -66,32 +64,32 @@ const Info = (props: InfoProps) => {
 			<div className="my-info__top">
 				<div className="my-info__group">
 					<div className="my-info__title--top">총자산</div>
-					<div className="my-info__data--top">₩ {info?.totalAssets.toLocaleString() || '-'}</div>
+					<div className="my-info__data--top">₩ {info?.totalAssets.toLocaleString() ?? '-'}</div>
 				</div>
 				<div className="my-info__group">
 					<div className="my-info__title--top">수익률</div>
-					<div className={`my-info__data--top${info?.status || ''}`}>
-						{info?.totalRate.toLocaleString(undefined, { maximumFractionDigits: 2 }) || '-'} %
+					<div className={infoDataClass('my-info__data--top', info?.totalRate ?? 0)}>
+						{info?.totalRate.toLocaleString(undefined, { maximumFractionDigits: 2 }) ?? '-'} %
 					</div>
 				</div>
 			</div>
 			<div className="my-info__bottom">
 				<div className="my-info__group">
 					<div className="my-info__title--bottom">현금자산</div>
-					<div className="my-info__data--bottom">₩ {formatInteger(info?.balance || 0)}</div>
+					<div className="my-info__data--bottom">₩ {formatInteger(info?.balance ?? 0)}</div>
 				</div>
 				<div className="my-info__group">
 					<div className="my-info__title--bottom">총매수금액</div>
-					<div className="my-info__data--bottom">₩ {formatInteger(info?.totalAskPrice || 0)}</div>
+					<div className="my-info__data--bottom">₩ {formatInteger(info?.totalAskPrice ?? 0)}</div>
 				</div>
 				<div className="my-info__group">
 					<div className="my-info__title--bottom">총평가금액</div>
-					<div className="my-info__data--bottom">₩ {formatInteger(info?.totalValuationPrice || 0)}</div>
+					<div className="my-info__data--bottom">₩ {formatInteger(info?.totalValuationPrice ?? 0)}</div>
 				</div>
 				<div className="my-info__group">
 					<div className="my-info__title--bottom ">총평가손익</div>
-					<div className={`my-info__data--bottom ${info?.status || ''}`}>
-						₩ {formatInteger(info?.totalValuationProfit || 0)}
+					<div className={infoDataClass('my-info__data--bottom', info?.totalRate ?? 0)}>
+						₩ {formatInteger(info?.totalValuationProfit ?? 0)}
 					</div>
 				</div>
 			</div>
